96e8770469004dd26580c1e4e40c8389a61180a7
Consuelo Gomez <mconsuelo.gomezt@gmail.com>
2022-06-29 12:19:37 -0400

    diseño base (sin logica aun)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+//npm init
+//npm start
+//cambiar en package.json el primer archivo a leer el index.js
+//agregar el type modules en el archivo a leer
+//instalar nodemon
+//ejecutar nodemon index.js
+
 import validator from './validator.js';
 
 console.log(validator);
+
+
+
