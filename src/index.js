bc3a1918b96531a313275b9977eddf84c7c141bb
Consuelo Gomez <mconsuelo.gomezt@gmail.com>
2022-07-01 22:08:43 -0400

    inicio de lógica (validar cuántos numeros se ingresaron)

--- src/index.js
+++ src/index.js
@@ -6,8 +6,34 @@
 //ejecutar nodemon index.js
 
 import validator from './validator.js';
-
 console.log(validator);
+
+//recoger los numeros de html como un array
+let recoger=document.getElementById('boton')
+recoger.addEventListener('click', pickUp)
+
+
+function pickUp(event) {
+    event.preventDefault();
+    let numeros = document.getElementById("numeros").value;  //console.log(numeros)
+    creditCardNumber = numeros;  //console.log(creditCardNumber)
+    //asegurar que se estan ingresando 16 numeros
+    let creditCardNumber = 0;
+  if (creditCardNumber.length != 16) {
+      let respuesta = document.getElementById("respuesta");
+      respuesta.innerHTML = "Debes ingresar los 16 números exactos de tu tarjeta";
+    } else {
+        respuesta.innerHTML = "good";
+    }
+    console.log(creditCardNumber.length); //comprobar la cantidad de números ingresados.
+}
+
+//mostrar contenido container de respuesta
+let show = document.getElementById('boton')
+show.onclick=function showDiv() {
+    document.getElementById("valido").style.display = "block";
+}
+
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,12 +1,8 @@
 const validator = {
-  // let numeros = document.getElementById("numeros");
-  // let boton = document.getElementById("boton");
-  // boton.addEventListener("click", () => {
-  //     let tarjeta = numeros.value.split("");
-  //     valor.push(tarjeta);
-  //     console.log(tarjeta);
-  // });
+  isValid(creditCardNumber) {
+  },
+  maskify(creditCardNumber) {
+  }
 };
 
 export default validator;
-
