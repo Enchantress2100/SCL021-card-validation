311a784a97f31122c9512654fb320d78a7291fc7
Consuelo Gomez <mconsuelo.gomezt@gmail.com>
2022-07-01 22:13:06 -0400

    recoger info del html como variable.

--- src/index.js
+++ src/index.js
@@ -12,18 +12,17 @@ console.log(validator);
 let recoger=document.getElementById('boton')
 recoger.addEventListener('click', pickUp)
 
-
 function pickUp(event) {
     event.preventDefault();
     let numeros = document.getElementById("numeros").value;  //console.log(numeros)
     creditCardNumber = numeros;  //console.log(creditCardNumber)
     //asegurar que se estan ingresando 16 numeros
     let creditCardNumber = 0;
-  if (creditCardNumber.length != 16) {
+    if (creditCardNumber.length != 16) {
       let respuesta = document.getElementById("respuesta");
       respuesta.innerHTML = "Debes ingresar los 16 números exactos de tu tarjeta";
     } else {
-        respuesta.innerHTML = "good";
+        validator.isValid(creditCardNumber)
     }
     console.log(creditCardNumber.length); //comprobar la cantidad de números ingresados.
 }
