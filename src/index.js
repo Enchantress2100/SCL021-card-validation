0530c15e0870b29bacf5051c47316d48ab01ba02
Consuelo Gomez <mconsuelo.gomezt@gmail.com>
2022-07-11 14:30:52 -0400

    cambios en input para tarjetas con menos numeros

--- src/index.js
+++ src/index.js
@@ -15,15 +15,19 @@ let creditCardNumber = 0; // scope global
 function pickUp(event) {
   event.preventDefault();
   let numeros = document.getElementById("numeros").value;
-
     // document.getElementById("demo").innerHTML = "I have changed!";console.log(numeros)
-    creditCardNumber = numeros;
+  creditCardNumber = numeros;
+  
+  //PASAR TARJETAS DE MENOS NUMEROS QUE 16, POR EJEMPLO AMEX, INTERPOLANDO UN ESPACIO VACIO AL INICIO PARA CUMPLIR CON LOS 16 DIGITOS (CADA VEZ QUE NO SE CUMPLE, VUELVE A AGREGAR)
+  while (creditCardNumber.length < 16) {
+     creditCardNumber= ` ${creditCardNumber}`
+  }
     // console.log(creditCardNumber)
     // asegurar que se estan ingresando 16 numeros
     // imprimir los numeros en la tarjeta separados cada 4 espacios (si es necesario)
-    // ejecutar validacion
+    // ejecutar validacion de todas las que no son AMEX
     // ejecutar funcion maskify
-    if (creditCardNumber.length === null || NaN) {
+    if (!creditCardNumber.length||isNaN(creditCardNumber) ) {
         let respuesta;
         respuesta = document.getElementById("respuesta");
         respuesta.innerHTML =
@@ -41,7 +45,7 @@ function pickUp(event) {
       validator.maskify(creditCardNumber);
   }
   // comprobar la cantidad de números ingresados.
-  // console.log(creditCardNumber.length);
+   console.log(creditCardNumber.length);
 }
 
 // mostrar contenido container de respuesta
@@ -49,5 +53,3 @@ let show = document.getElementById('boton')
 show.onclick=function showDiv() {
     document.getElementById("valido").style.display = "block";
 }
-
-module.exports=pickUp(event)
