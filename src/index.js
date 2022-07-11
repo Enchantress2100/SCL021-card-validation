08e2f165561fda8cef69b2caa86d9e2cd1ba675a
Consuelo Gomez <mconsuelo.gomezt@gmail.com>
2022-07-11 13:03:52 -0400

    cambios en regex

--- src/index.js
+++ src/index.js
@@ -1,52 +1,53 @@
-//npm init
-//npm start
-//cambiar en package.json el primer archivo a leer el index.js
-//agregar el type modules en el archivo a leer
-//instalar nodemon
-//ejecutar nodemon index.js
+// npm init
+// npm start
+// cambiar en package.json el primer archivo a leer el index.js
+// agregar el type modules en el archivo a leer
 
 import validator from './validator.js';
-console.log(validator);
+// console.log(validator);
 
-//recoger los numeros de html como un array
+// recoger los numeros de html como un array
 let recoger=document.getElementById('boton')
 recoger.addEventListener('click', pickUp)
 
-let creditCardNumber = 0; //scope global
+let creditCardNumber = 0; // scope global
 
 function pickUp(event) {
   event.preventDefault();
   let numeros = document.getElementById("numeros").value;
 
-    //document.getElementById("demo").innerHTML = "I have changed!";console.log(numeros)
+    // document.getElementById("demo").innerHTML = "I have changed!";console.log(numeros)
     creditCardNumber = numeros;
-    //console.log(creditCardNumber)
-    //asegurar que se estan ingresando 16 numeros
-    //imprimir los numeros en la tarjeta separados cada 4 espacios (si es necesario)
-    //ejecutar validacion
-    //ejecutar funcion maskify
-    if (creditCardNumber.length != 16) {
-        let respuesta = document.getElementById("respuesta");
+    // console.log(creditCardNumber)
+    // asegurar que se estan ingresando 16 numeros
+    // imprimir los numeros en la tarjeta separados cada 4 espacios (si es necesario)
+    // ejecutar validacion
+    // ejecutar funcion maskify
+    if (creditCardNumber.length === null || NaN) {
+        let respuesta;
+        respuesta = document.getElementById("respuesta");
         respuesta.innerHTML =
-        "Debes revisar e ingresar los 16 números exactos de tu tarjeta";
+        "Debes revisar e ingresar los números exactos de tu tarjeta";
     } else {
-        //let tarjeta = document.getElementById('numeroTarjeta');
+        // let tarjeta = document.getElementById('numeroTarjeta');
         // const separate = 4;
         // numeros = numeros
         //   .split("")
         //   .map((x, i) => (i > 0 && i % separate == 0 ? "-" + x : x))
         //   .join("");
-        //tarjeta.innerHTML=numeros
+        // tarjeta.innerHTML=numeros
       
       validator.isValid(creditCardNumber);
       validator.maskify(creditCardNumber);
   }
-  //comprobar la cantidad de números ingresados.
-  //console.log(creditCardNumber.length);
+  // comprobar la cantidad de números ingresados.
+  // console.log(creditCardNumber.length);
 }
 
-//mostrar contenido container de respuesta
+// mostrar contenido container de respuesta
 let show = document.getElementById('boton')
 show.onclick=function showDiv() {
     document.getElementById("valido").style.display = "block";
 }
+
+module.exports=pickUp(event)
--- src/validator.js
+++ src/validator.js
@@ -1,68 +1,68 @@
 const validator = {
   isValid(creditCardNumber) {
-    //console.log(creditCardNumber)
-    //transformar a un array
+    // console.log(creditCardNumber)
+    // transformar a un array
     let arrayCreditCard = String(creditCardNumber)
       .split("")
       .map((creditCardNumber) => {
         return Number(creditCardNumber);
       });
-    //console.log(arrayCreditCard);
-    //filtrar buscando las posiciones pares
+    // console.log(arrayCreditCard);
+    // filtrar buscando las posiciones pares
     let filterEven = arrayCreditCard.filter((number, index) => {
       return index % 2 === 0;
     });
-    //console.log(filterEven);
-    //filtrar buscando las posiciones impares
+    // console.log(filterEven);
+    // filtrar buscando las posiciones impares
     let filterUneven = arrayCreditCard.filter((number, index) => {
       return index % 2 !== 0;
     });
-    //console.log(filterUneven);
-    //ejecutar la multiplicacion *2 en las posiciones pares del array, desde 0
+    // console.log(filterUneven);
+    // ejecutar la multiplicacion *2 en las posiciones pares del array, desde 0
     let evenMulti = filterEven.map(function (currentNumber, index) {
       return currentNumber * 2;
     });
-   //console.log(evenMulti);
+   // console.log(evenMulti);
 
-    //filtrar resultados mayores a 9 de la multiplicacion
+    // filtrar resultados mayores a 9 de la multiplicacion
     let findValue = evenMulti.filter((i) => i > 9);
-    //console.log(findValue);
+    // console.log(findValue);
 
-    //estructurar digitos individuales
+    // estructurar digitos individuales
     let digits = findValue.toString().split("");
-    //eliminar los nAn
+    // eliminar los nAn
     let realDigits = digits.map(Number).filter(Number);
-    //console.log(realDigits);
+    // console.log(realDigits);
 
-    //sumar los digitos entre si
+    // sumar los digitos entre si
     function sumar(realDigits) {
       let suma = 0;
       for (let i = 0; i < realDigits.length; i++) {
         suma += parseInt(realDigits[i]);
       }
-      //console.log(suma)
+      // console.log(suma)
     }
     sumar(realDigits)
 
-    //se deben sumar todos los numeros del array
-    //reunir los arrays
-    //evenMulti
-    //realDigits
+    // se deben sumar todos los numeros del array
+    // reunir los arrays
+    // evenMulti
+    // realDigits
     let arrayEven = evenMulti.concat(realDigits).filter((i) => i < 9);
-    //console.log(arrayEven)
-    //agregar filterUneven
+    // console.log(arrayEven)
+    // agregar filterUneven
     let arrayDef = arrayEven.concat(filterUneven)
-    //console.log(arrayDef)
-    //sumar los digitos
+    // console.log(arrayDef)
+    // sumar los digitos
        function sumarFinal(arrayDef) {
          let sumar = 0;
          for (let i = 0; i < arrayDef.length; i++) {
            sumar += parseInt(arrayDef[i]);
          }
-         //console.log(sumar);
+         // console.log(sumar);
          return sumar
        }
-    //si el numero final es multiplo de 10, la tarjeta es valida.
+    // si el numero final es multiplo de 10, la tarjeta es valida. 
     let final = sumarFinal(arrayDef)
     
     if ((final % 10) == 0) {
@@ -72,9 +72,10 @@ const validator = {
     }
   },
   maskify(creditCardNumber) {
-    let tarjeta = document.getElementById("numeroTarjeta");
+    let tarjeta;
+    tarjeta = document.getElementById("numeroTarjeta");
     tarjeta.innerHTML = creditCardNumber.replace(
-      /\b(?:\d{4}[ -]?){3}(?=\d{4}\b)/,"#### #### #### "
+      /\d(?=\d{4})/mg,"#"
     );
   },
 };
